8a20b1da0f7d9ec3a8654feb11be153cac25eb61
qvil <tshyeon@seowonintech.co.kr>
2018-05-16 11:00:46 +0900

    Add type to function
    * tsconfig.json : include: src, outDir: dist
    * Install tsc-watch --dev

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "qvil", age = 27, gender = "mael";
 const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
 };
-sayHi(name, age);
+console.log(sayHi("qvil", 27, "male"));
 //# sourceMappingURL=index.js.map
