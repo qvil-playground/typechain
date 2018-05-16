69a4954c07977e965afeca888b4a1840ed55adae
qvil <tshyeon@seowonintech.co.kr>
2018-05-16 10:46:08 +0900

    Optional parameter test

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hihi");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "qvil", age = 27, gender = "mael";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hihi")
+const name = "qvil",
+  age = 27,
+  gender = "mael";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
