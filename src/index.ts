8fcad858e3bb2f69fd564d441dbabe7855a8e1e6
qvil <tshyeon@seowonintech.co.kr>
2018-05-17 09:25:07 +0900

    #6 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  // private age: number; // Property 'age' is private and only accessible within class 'Human'.
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
+
+const yelg = new Human("Yelg", 39, "mail");
 
 const person = {
   name: "qvil",
@@ -11,9 +20,11 @@ const person = {
 };
 
 const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+  return `Hello ${person.name}, you are ${person.age}, you are a ${
+    person.gender
+  }`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(yelg));
 
 export {};
